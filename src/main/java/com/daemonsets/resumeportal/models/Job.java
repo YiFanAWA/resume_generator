aa1b5c4428ce9e838228e2f479b466eba75f0777
YiFanAWA <202412214801011@zcmu.edu.cn>
2026-06-01 16:18:37 +0800

    fix: 更新配置文件支持 MySQL 8.0 和 Spring Boot 2.3+

--- src/main/java/com/daemonsets/resumeportal/models/Job.java
+++ src/main/java/com/daemonsets/resumeportal/models/Job.java
@@ -1,102 +1,44 @@
 package com.daemonsets.resumeportal.models;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.time.format.FormatStyle;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Table
+@Getter
+@Setter
+@NoArgsConstructor
 public class Job {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     private String company;
     private String designation;
+
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate startDate;
+
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate endDate;
+
     private boolean isCurrentJob;
-    @ElementCollection(targetClass=String.class)
-    private List<String> responsibilities = new ArrayList();
 
-    public List<String> getResponsibilities() {
-        return responsibilities;
-    }
-
-    public void setResponsibilities(List<String> responsibilities) {
-        this.responsibilities = responsibilities;
-    }
-
-    public boolean isCurrentJob() {
-        return isCurrentJob;
-    }
-
-    public void setCurrentJob(boolean currentJob) {
-        isCurrentJob = currentJob;
-    }
-
-    @Override
-    public String toString() {
-        return "Job{" +
-                "id=" + id +
-                ", company='" + company + '\'' +
-                ", designation='" + designation + '\'' +
-                ", startDate=" + startDate +
-                ", endDate=" + endDate +
-                '}';
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getCompany() {
-        return company;
-    }
-
-    public void setCompany(String company) {
-        this.company = company;
-    }
-
-    public String getDesignation() {
-        return designation;
-    }
-
-    public void setDesignation(String designation) {
-        this.designation = designation;
-    }
-
-    public LocalDate getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(LocalDate startDate) {
-        this.startDate = startDate;
-    }
-
-    public LocalDate getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(LocalDate endDate) {
-        this.endDate = endDate;
-    }
+    @ElementCollection(targetClass = String.class)
+    private List<String> responsibilities = new ArrayList<>();
 
     public String getFormattedStartDate() {
-        return startDate.format(DateTimeFormatter.ofPattern("MMM yyyy"));
+        return startDate != null ? startDate.format(DateTimeFormatter.ofPattern("MMM yyyy")) : null;
     }
 
     public String getFormattedEndDate() {
-        return endDate.format(DateTimeFormatter.ofPattern("MMM yyyy"));
+        return endDate != null ? endDate.format(DateTimeFormatter.ofPattern("MMM yyyy")) : null;
     }
 }
